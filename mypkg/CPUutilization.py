d9469e69490e52946d6b78a9a319bd6684cf9945
Hikaru Nemoto <s23C1105ZV@s.chibakoudai.jp>
2025-01-05 01:44:58 +0900

    make a CPUutilization

---
+++ mypkg/CPUutilization.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2025 Hikaru Nemoto 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy
+import subprocess
+from rclpy.node import Node
+from std_msgs.msg import String
+
+def get_cpu_usage():
+    result = subprocess.run("vmstat 1 2 | awk 'NR==3 {print $13, $14, $15}'", capture_output=True, text=True, shell=True)
+    output = result.stdout.strip().split()
+    
+    if len(output) == 3:
+        user_cpu = output[0]
+        system_cpu = output[1]
+        idle_cpu = output[2]
+        
+        return f"User CPU: {user_cpu}%, System CPU: {system_cpu}%, Idle CPU: {idle_cpu}%"
+    else:
+        return "Failed to retrieve CPU usage data."
+
+def cb():
+    cpu_usage = get_cpu_usage()
+    msg = String()
+    msg.data = cpu_usage
+    pub.publish(msg)
+    node.get_logger().info(f"Published CPU usage data:\n{msg.data}")
+
+def main():
+    rclpy.init()
+    global node, pub
+    node = Node("CPUutilization")
+    pub = node.create_publisher(String, "cpu_usage", 10)
+    node.create_timer(3.0, cb)
+    rclpy.spin(node)
+
--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
-            'listener = mypkg.listener:main',
+            'cpuutilization = mypkg.CPUutilization:main',
         ],
     },
 )
