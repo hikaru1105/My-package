61d3fb60db0698b47525867ad24c8196cbde20e1
Hikaru Nemoto <s23C1105ZV@s.chibakoudai.jp>
2025-01-12 17:45:45 +0900

    ファイル名の変更

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cpuutilization = mypkg.CPUutilization:main',
+            'cpuutilization = mypkg.cpuutilization:main',
         ],
     },
 )
